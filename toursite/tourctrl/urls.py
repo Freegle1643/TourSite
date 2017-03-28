2d04dbe52cf42bb50c945294c710442bcd6ef65e
Yuan Hao <pearl1643@live.com>
2017-03-28 20:35:52 +0800

    现在可以在Debug=True的时候在编辑框内预览上传的图片了
    主要是在urls.py中加入几行代码，核心是告诉django这里我可以返回文件，具体可以见文档https://docs.djangoproject.com/en/1.9/ref/views/#serving-files-in-development

--- toursite/tourctrl/urls.py
+++ toursite/tourctrl/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from .views import *
 
@@ -31,3 +32,10 @@ urlpatterns = [
     url(r'^feature_north/$',feature_north),
     url(r'^editjournal/imageupload/$',ImageUpload)
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
